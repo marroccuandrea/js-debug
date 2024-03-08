52f849ffc0e3e0e8b37939969668b6dcf9163cde
marroccuandrea <marroccuandrea@yahoo.it>
2024-03-08 17:16:54 +0100

    bundle 1 Es.4

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -27,10 +27,12 @@ function addIfEven(num) {
     return num;
 }
 
+addIfEven();
+
 // 1. Questo codice esprime una funzione che selezionerà solo numeri pari e lo restituirà aggiungendo 5
 // 2. Ci sono errori di sintassi, nell'operatore: num % 2 = 0, risulterà sempre pari e quindi aggiungerà sempre 5, anche ai numeri dispari.
 // 2. La soluzione sarà mettere due == anzichè uno solo
-// 3. Non ci sono errori logici
+// 3. La funzione non viene richiamata
 
 
 // ESERCIZIO 3
@@ -39,20 +41,28 @@ function loopToFive() {
         console.log(i);
     }
 }
+
+loopToFive();
 // 1. Questo esercizio esprime una funzione che genera numeri da 0 a 4
 // 2. Sono presenti errori di sintassi, al posto delle virgole nel for ci va il punto e virgola
-// 3. Non sono presenti errrori di logica
+// 3. La funzione non viene richiamata
 
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 == 0) {
+            evenNumbers.push(numbers[i]);
         }
         return evenNumbers;
     }
 }
 displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
+
+// 1. Questa funzione dovrebbe restituire solo i numeri pari all'interno dell'array
+// 2. Ci sono errori di sintassi nella riga 51, dopo i++ non va il punto e virgola
+// 2. Un altro errore di sintassi è nella riga 52, ci vuole il simbolo di uguaglianza, non un semplice uguale, anche il punto e virgola dopo l'if non è corretto
+// 3. numbers.length -1 è sbagliato perchè non mi darà mai il numero 8, il return deve esesere spostato fuori dall'if, infine numbers nell'if andava messo a indice di i
